4eb968c4474befed934fff1847234806fd2d790c
Bradley Wright <bradley.wright@gmail.com>
2022-02-14 11:39:47 -0500

    Colors and sizes

---
+++ src/utils/constants.js
@@ -0,0 +1,82 @@
+export const planets = [
+  {
+    name: 'MERCURY',
+    color: '#419EBB',
+    dotColor: '#DEF4FC',
+    imageSizes: {
+      mobile: 111,
+      tablet: 184,
+      desktop: 290
+    }
+  },
+  {
+    name: 'VENUS',
+    color: '#EDA249',
+    dotColor: '#F7CC7F',
+    imageSizes: {
+      mobile: 154,
+      tablet: 184,
+      desktop: 400
+    }
+  },
+  {
+    name: 'EARTH',
+    color: '#6F2ED6',
+    dotColor: '#545BFE',
+    imageSizes: {
+      mobile: 173,
+      tablet: 285,
+      desktop: 450
+    }
+  },
+  {
+    name: 'MARS',
+    color: '#D14C32',
+    dotColor: '#FF6A45',
+    imageSizes: {
+      mobile: 129,
+      tablet: 213,
+      desktop: 336
+    }
+  },
+  {
+    name: 'JUPITER',
+    color: '#D83A34',
+    dotColor: '#ECAD7A',
+    imageSizes: {
+      mobile: 224,
+      tablet: 369,
+      desktop: 582
+    }
+  },
+  {
+    name: 'SATURN',
+    color: '#CD5120',
+    dotColor: '#FCCB6B',
+    imageSizes: {
+      mobile: 256,
+      tablet: 422,
+      desktop: 666
+    }
+  },
+  {
+    name: 'URANUS',
+    color: '#1EC1A2',
+    dotColor: '#65F0D5',
+    imageSizes: {
+      mobile: 176,
+      tablet: 290,
+      desktop: 458
+    }
+  },
+  {
+    name: 'NEPTUNE',
+    color: '#2D68F0',
+    dotColor: '#497EFA',
+    imageSizes: {
+      mobile: 173,
+      tablet: 285,
+      desktop: 450
+    }
+  },
+]
