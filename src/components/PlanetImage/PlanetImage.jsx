53a7860a7a1c584593622c94ac202658ab53b3b7
Bradley Wright <bradley.wright@gmail.com>
2022-02-14 15:02:17 -0500

    Automatic image sizing

--- src/components/PlanetImage/PlanetImage.jsx
+++ src/components/PlanetImage/PlanetImage.jsx
@@ -1,13 +1,17 @@
+import { planets } from 'utils/constants'
 import data from 'data.json'
 
 export default function PlanetImage({ planet, view }) {
-  let images = require.context('../..', true)
-  let img = images(`./${data[planet].images.planet.slice(2)}`)
+  const images = require.context('../..', true)
 
   return (
     <div className="planet-image">
-      {/* <img src={require('../../assets/planet-earth.svg').default} alt={data[planet].name} /> */}
-      <img src={img} alt={data[planet].name} />
+      <img
+        src={images(`./${data[planet].images.planet.slice(2)}`)}
+        alt={data[planet].name}
+        height={planets[planet].imageSizes.mobile}
+        width={planets[planet].imageSizes.mobile}
+      />
     </div>
   )
 }
