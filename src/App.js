5aab7f67097488a15d0d532fc38589f1aef95e8c
Bradley Wright <bradley.wright@gmail.com>
2022-02-14 10:45:41 -0500

    Arranging stubs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import HamburgerMenu from "components/HamburgerMenu/HamburgerMenu";
+import PlanetImage from "components/PlanetImage/PlanetImage";
+import PlanetMenu from "components/PlanetMenu/PlanetMenu";
+import StatsBar from "components/StatsBar/StatsBar";
+import SummaryCard from "components/SummaryCard/SummaryCard";
+import Title from "components/Title/Title";
+import ViewSelector from "components/ViewSelector/ViewSelector";
+
 export default function App() {
   return (
     <div className="App">
-     
+      <Title />
+      <HamburgerMenu />
+      <PlanetMenu />
+      <ViewSelector />
+      <PlanetImage />
+      <SummaryCard />
+      <StatsBar />
     </div>
   );
 }
