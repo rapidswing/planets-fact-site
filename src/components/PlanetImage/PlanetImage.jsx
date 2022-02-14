8f885fb46b0be5e00b816a8b015599952ec3d1cc
Bradley Wright <bradley.wright@gmail.com>
2022-02-14 16:54:49 -0500

    Matching view/image

--- src/components/PlanetImage/PlanetImage.jsx
+++ src/components/PlanetImage/PlanetImage.jsx
@@ -6,10 +6,27 @@ import './PlanetImage.scss'
 export default function PlanetImage({ planet, view }) {
   const images = require.context('../..', true)
 
+  const getImage = (type) => {
+    switch(type) {
+      case 'OVERVIEW': {
+        return data[planet].images.planet.slice(2);
+      }
+      case 'STRUCTURE': {
+        return data[planet].images.internal.slice(2);
+      }
+      case 'SURFACE':
+      default: {
+        return data[planet].images.planet.slice(2);
+      }
+    }
+    
+  }
+
   return (
     <div className="planet-image">
       <img
-        src={images(`./${data[planet].images.planet.slice(2)}`)}
+        // src={images(`./${data[planet].images.planet.slice(2)}`)}
+        src={images(`./${getImage(view)}`)}
         alt={data[planet].name}
         height={planets[planet].imageSizes.mobile}
         width={planets[planet].imageSizes.mobile}
