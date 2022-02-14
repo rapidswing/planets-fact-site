3b86c0e911e8749bf3dee26b4e376f7355284afe
Bradley Wright <bradley.wright@gmail.com>
2022-02-14 13:53:49 -0500

    Added planet image imports

--- src/components/PlanetImage/PlanetImage.jsx
+++ src/components/PlanetImage/PlanetImage.jsx
@@ -1,5 +1,36 @@
-export default function PlanetImage() {
+import Mercury from 'assets/planet-mercury.svg'
+import Venus from 'assets/planet-venus.svg'
+import Earth from 'assets/planet-earth.svg'
+import Mars from 'assets/planet-mars.svg'
+import Jupiter from 'assets/planet-jupiter.svg'
+import Saturn from 'assets/planet-saturn.svg'
+import Uranus from 'assets/planet-uranus.svg'
+import Neptune from 'assets/planet-neptune.svg'
+
+import MercuryInternal from 'assets/planet-mercury-internal.svg'
+import VenusInternal from 'assets/planet-venus-internal.svg'
+import EarthInternal from 'assets/planet-earth-internal.svg'
+import MarsInternal from 'assets/planet-mars-internal.svg'
+import JupiterInternal from 'assets/planet-jupiter-internal.svg'
+import SaturnInternal from 'assets/planet-saturn-internal.svg'
+import UranusInternal from 'assets/planet-uranus-internal.svg'
+import NeptuneInternal from 'assets/planet-neptune-internal.svg'
+
+import MercuryGeology from 'assets/geology-mercury.png'
+import VenusGeology from 'assets/geology-venus.png'
+import EarthGeology from 'assets/geology-earth.png'
+import MarsGeology from 'assets/geology-mars.png'
+import JupiterGeology from 'assets/geology-jupiter.png'
+import SaturnGeology from 'assets/geology-saturn.png'
+import UranusGeology from 'assets/geology-uranus.png'
+import NeptuneGeology from 'assets/geology-neptune.png'
+
+import { planets } from 'utils/constants'
+
+export default function planetImage({ index }) {
   return (
-    <div className="planet-image"></div>
+    <div className="planet-image">
+
+    </div>
   )
 }
