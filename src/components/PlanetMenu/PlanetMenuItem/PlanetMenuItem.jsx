41c2396b0b035a43ac95358859bee399e3f03466
Bradley Wright <bradley.wright@gmail.com>
2022-02-15 01:20:28 -0500

    Desktop menu view color

--- src/components/PlanetMenu/PlanetMenuItem/PlanetMenuItem.jsx
+++ src/components/PlanetMenu/PlanetMenuItem/PlanetMenuItem.jsx
@@ -7,9 +7,9 @@ import './PlanetMenuItem.scss'
 export default function PlanetMenuItem({ planet }) {
   return (
     <>
-    <div className="planet-menu-item" data-planet={planet} >
+    <div className={`planet-menu-item color-${planet}`} data-planet={planet}>
       <div className="planet-dot" style={{ background: planets[planet].dotColor }}></div>
-      <div className="planet-name">{planets[planet].name}</div>
+      <div className='planet-name'>{planets[planet].name}</div>
       <div className="planet-chevron">
         <img src={Chevron} alt="Chevron" />
       </div>
