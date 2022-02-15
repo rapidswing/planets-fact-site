5710d263f0343ac5310194d7eed823cad528dff1
Bradley Wright <bradley.wright@gmail.com>
2022-02-14 20:13:34 -0500

    Moved SVG h/w info into CSS

--- src/components/PlanetImage/PlanetImage.jsx
+++ src/components/PlanetImage/PlanetImage.jsx
@@ -1,4 +1,3 @@
-import { planets } from 'utils/constants'
 import data from 'data.json'
 
 import './PlanetImage.scss'
@@ -25,11 +24,9 @@ export default function PlanetImage({ planet, view }) {
   return (
     <div className="planet-image">
       <img
-        // src={images(`./${data[planet].images.planet.slice(2)}`)}
+        className={`planet-image-${planet}`}
         src={images(`./${getImage(view)}`)}
         alt={data[planet].name}
-        height={planets[planet].imageSizes.mobile}
-        width={planets[planet].imageSizes.mobile}
       />
     </div>
   )
